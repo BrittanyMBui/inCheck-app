45de1332d6df2e988153c9214d36cd2c364085e4
brittanymbui <brittymbui@gmail.com>
2021-02-13 09:08:56 -0800

    Adds create, view, edit todo templates, adds ToDo model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+TITLES = (
+    ('P', 'Projects'),
+    ('H', 'Household'),
+    ('S', 'Shopping'),
+    ('F', 'Finances'),
+    ('W', 'Work'),
+    ('T', 'Trips'),
+    ('M', 'Misc')
+)
+class ToDo(models.Model):
+    title = models.CharField(
+        max_length=1,
+        choices=TITLES,
+        default=TITLES[0][0]
+        )
+    date_created = models.DateField()
+    due_date = models.DateField()
+    body = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-date_created']
+
+
+
