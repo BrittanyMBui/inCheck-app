689664ae7e18c7ca1c1c92322135630168497cbb
brittanymbui <brittymbui@gmail.com>
2021-02-13 15:13:06 -0800

    Adds edit, delete templates, back buttons, log out function

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('incheck/', views.incheck_index, name='incheck_index'),
     path('incheck/<int:todo_id>', views.view_todo, name='view_todo'),
+    path('incheck/create/', views.create_todo, name='create_todo'),
+    path('incheck/<int:todo_id>/delete/', views.delete_todo, name='delete_todo'),
+    path('incheck/<int:todo_id>/edit', views.edit_todo, name='edit_todo'),
     # Auth
     path('accounts/', views.signup, name='signup')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import ToDo
+from .forms import ToDoForm
 
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def incheck_index(request):
     todos = ToDo.objects.filter(user=request.user)
     return render(request, 'incheck_index.html', {'todos': todos})
@@ -26,6 +28,7 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
+@login_required
 def view_todo(request, todo_id):
     todo = ToDo.objects.get(id=todo_id)
     context = {
@@ -33,3 +36,43 @@ def view_todo(request, todo_id):
     }
 
     return render(request, 'todos/view_todo.html', context)
+
+@login_required
+def create_todo(request):
+    if request.method == "POST":
+        todo_form = ToDoForm(request.POST)
+        if todo_form.is_valid():
+            new_todo = todo_form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            return redirect('incheck_index')
+
+    todo_form = ToDoForm()
+    context = {
+        'todo_form': todo_form
+    }
+    return render(request, 'todos/create_todo.html', context)
+
+@login_required
+def delete_todo(request, todo_id):
+    if request.method == "POST":
+        todo = ToDo.objects.get(id=todo_id)
+        todo.delete()
+        return redirect('incheck_index')
+
+@login_required
+def edit_todo(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+
+    if request.method == "GET":
+        todo_form = ToDoForm(instance=todo)
+        context = {
+            'form': todo_form
+        }
+        return render(request, 'todos/edit_todo.html', context)
+    
+    else:
+        todo_form = ToDoForm(request.POST, instance=todo)
+        if todo_form.is_valid():
+            todo_form.save()
+            return redirect('view_todo', todo_id=todo_id)
