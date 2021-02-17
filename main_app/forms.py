8170635b6ad7c4dc55acdea57b1818cb498eb638
brittanymbui <brittymbui@gmail.com>
2021-02-17 08:44:38 -0800

    adds new forms, adds to base, home, index, todo templates

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,4 +4,4 @@ from .models import ToDo
 class ToDoForm (forms.ModelForm):
     class Meta:
         model = ToDo
-        fields = ['name', 'date_created', 'title', 'due_date', 'body', 'completed']
+        fields = ['name', 'date_created', 'title', 'due_date', 'body']
--- main_app/models.py
+++ main_app/models.py
@@ -22,7 +22,6 @@ class ToDo(models.Model):
     date_created = models.DateField()
     due_date = models.DateField()
     body = models.CharField(max_length=250)
-    completed = models.BooleanField(default = False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -30,6 +29,24 @@ class ToDo(models.Model):
     
     class Meta:
         ordering = ['-date_created']
+
+IMPORTANCE = (
+    ('L', 'Low'),
+    ('M', 'Moderate'),
+    ('H', 'High'),
+)
+
+class Importance(models.Model):
+    importance = models.CharField(
+        max_length=1,
+        choices=IMPORTANCE,
+        default=IMPORTANCE[0][0]
+    )
+
+    todo = models.ForeignKey(ToDo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_importance_display()}'
 
 
 
